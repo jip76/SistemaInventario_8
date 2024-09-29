d4e3343fe4c68eefd39b5cf09d424ecf420ec400
jorgeplaza <jip76@hotmail.com>
2024-09-28 23:44:05 -0500

    Carro de Compras - Modelo y Repositorio

---
+++ SistemaInventario.AccesoDatos/Migrations/20240928200350_AgregarCarroComprasMigracion.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCarroComprasMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Companias",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Companias",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateTable(
+                name: "CarroCompras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioAplicacionId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarroCompras", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarroCompras_AspNetUsers_UsuarioAplicacionId",
+                        column: x => x.UsuarioAplicacionId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CarroCompras_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarroCompras_ProductoId",
+                table: "CarroCompras",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarroCompras_UsuarioAplicacionId",
+                table: "CarroCompras",
+                column: "UsuarioAplicacionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarroCompras");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Companias",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Companias",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
