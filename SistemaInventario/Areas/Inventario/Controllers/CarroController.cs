61330aa8241625557cedaa546a26ffa1633ea157
jorgeplaza <jip76@hotmail.com>
2024-09-30 09:18:33 -0500

    Carro de Compras - Administracion

---
+++ SistemaInventario/Areas/Inventario/Controllers/CarroController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventario.Modelos.ViewModels;
+using SistemaInventario.Utilidades;
+
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace SistemaInventario.Areas.Inventario.Controllers
+{
+    [Area("Inventario")]
+    public class CarroController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        [BindProperty]
+        public CarroCompraVM carroCompraVM { get; set; }
+
+        public CarroController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        [Authorize]
+
+        public async Task<IActionResult> Index()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            carroCompraVM = new CarroCompraVM();
+            carroCompraVM.Orden = new Orden();
+            carroCompraVM.CarroCompralista = await _unidadTrabajo.CarroCompra.ObtenerTodos(u => u.UsuarioAplicacionId == claim.Value,
+                                                                                            incluirPropiedades:"Producto");
+            carroCompraVM.Orden.TotalOrden = 0;
+            carroCompraVM.Orden.UsuarioAplicacionId = claim.Value;
+
+            foreach (var lista in carroCompraVM.CarroCompralista)
+            {
+                lista.Precio = lista.Producto.Precio;// siempre mostrar el precio actual del producto
+                carroCompraVM.Orden.TotalOrden += (lista.Precio * lista.Cantidad);
+            }
+
+            return View(carroCompraVM);
+        }
+        public async Task<IActionResult> mas(int carroId)
+        {
+            var carrocompras = await _unidadTrabajo.CarroCompra.ObtenerPrimero(c => c.Id == carroId);
+            carrocompras.Cantidad += 1;
+            await _unidadTrabajo.Guardar();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> menos(int carroId)
+        {
+            var carrocompras = await _unidadTrabajo.CarroCompra.ObtenerPrimero(c => c.Id == carroId);
+            if (carrocompras.Cantidad == 1)
+            {
+                // Remover el registros del carro de compras y actualizaremos la session
+                var carroLista = await _unidadTrabajo.CarroCompra.ObtenerTodos(c => c.UsuarioAplicacionId == carrocompras.UsuarioAplicacionId);
+                var numeroproducto = carroLista.Count();
+                _unidadTrabajo.CarroCompra.Remover(carrocompras);
+                await _unidadTrabajo.Guardar();
+                HttpContext.Session.SetInt32(DS.ssCarroCompras, numeroproducto - 1);
+            }
+            else
+            {
+                carrocompras.Cantidad -= 1;
+                await _unidadTrabajo.Guardar();
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> remover(int carroId)
+        {
+            // Remueve el  registro del Carro de compra y actualiza la sesion
+            var carrocompras = await _unidadTrabajo.CarroCompra.ObtenerPrimero(c => c.Id == carroId);
+            var carroLista = await _unidadTrabajo.CarroCompra.ObtenerTodos(c => c.UsuarioAplicacionId == carrocompras.UsuarioAplicacionId);
+            var numeroproducto = carroLista.Count();
+            _unidadTrabajo.CarroCompra.Remover(carrocompras);
+            await _unidadTrabajo.Guardar();
+            HttpContext.Session.SetInt32(DS.ssCarroCompras, numeroproducto - 1);
+            return RedirectToAction("Index");
+        }
+    }
+}
